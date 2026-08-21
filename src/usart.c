abebf75fa774933cb9029ff085412ccc8b87ad4f
Hernan Silva <hernansilva2002@gmail.com>
2026-08-21 16:51:03 -0300

    costo de energia bien calculado

--- src/usart.c
+++ src/usart.c
@@ -98,8 +98,8 @@ void Send_Output(uint32_t* amps, uint32_t report_interval_acc) {
         }
         case FORMAT_COST:
         {
-            uint32_t full_cost = (amps[0]*1000U + amps[1]) * 220 * WH_PRICE * report_interval_acc / 3600;
-            len = snprintf(buffer, sizeof(buffer), "$%lu.%03lu %lu seg\r\n", full_cost / 1000U, full_cost % 1000U, report_interval_acc);
+            uint32_t full_cost = (amps[0]*1000UL + amps[1]) * 220UL * report_interval_acc;
+            len = snprintf(buffer, sizeof(buffer), "$%lu.%03lu %lu seg\r\n", full_cost / 1000UL, full_cost % 1000U, report_interval_acc);
             USART_WriteBlocking(USART0, buffer, len);
             break;
         }
